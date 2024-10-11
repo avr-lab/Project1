c8e490aaad0d2c8706bd0898488233df96dbd5ff
avr-lab <avr@hawk.iit.edu>
2024-10-11 00:03:29 -0500

    our_ElasticNet.py

--- elasticnet/models/ElasticNet.py
+++ elasticnet/models/ElasticNet.py
@@ -1,17 +1,5 @@
-
-
-class ElasticNetModel():
-    def __init__(self):
-        pass
-
-
-    def fit(self, X, y):
-        return ElasticNetModelResults()
-
-
-class ElasticNetModelResults():
-    def __init__(self):
-        pass
-
-    def predict(self, x):
-        return 0.5
+def elastic_net_loss(beta, X, y, alpha=0.001, l1_ratio=0.5):
+    ...
+loss = np.sum(residuals**2) / (2 * len(y)) + alpha * (l1_ratio * l1_penalty + (1 - l1_ratio) * l2_penalty)
+    return loss
+result = minimize(elastic_net_loss, initial_beta, args=(X, y, alpha, l1_ratio), method='BFGS')
